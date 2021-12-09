279ce05edfc555cadbd26fa8f983db7882d36618
GameHole <741419087@qq.com>
2021-12-09 14:45:51 +0800

    添加ios pbx 辅助方法

---
+++ Assets/AndrodBuildHelper/Editor/PBXHelper.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor.iOS.Xcode;
+namespace MiniGameSDK
+{
+    public static class PBXHelper
+    {
+        public static string GetPBXTargetGuid(this PBXProject project)
+        {
+#if UNITY_2019_3_OR_NEWER
+            string targetGUID = project.GetUnityFrameworkTargetGuid();
+#else
+            string targetName = PBXProject.GetUnityTargetName();
+            string targetGUID = project.TargetGuidByName(targetName);
+#endif
+            return targetGUID;
+        }
+    }
+}
